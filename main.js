32f947202c25e6f5c87e8fbaf18887e8b7ce9f67
Ashley Claiborne <languageisaliquid@gmail.com>
2019-08-31 11:43:20 -0500

    created project array and function to build cards

--- main.js
+++ main.js
@@ -1,1 +1,74 @@
 console.log('This works!');
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+const projects = [
+    {
+        title: "Cool Project", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: true,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+      },
+      {
+        title: "Cool Project", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: true,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+      },
+      {
+        title: "Cool Project", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: true,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+      },
+      {
+        title: "Cool Project", 
+        screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: true,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+      }
+];
+
+const createProjectCards = (projectArr) => {
+    let cardsToPrint = '';
+    // const availableProjects = [];
+    for (let i = 0; i < projectArr.length; i++) {
+        let projectObj = projectArr[i];
+        cardsToPrint += `
+            <div class="projectCard ${projectObj.available}">
+                <header><h4> ${projectObj.title} </h4></header>
+                <img src=${projectObj.screenshot} alt="${projectObj.title} screenshot" />
+                <p>${projectObj.description}</p>
+                <p>${projectObj.technologiesUsed}</p>
+                <p class="availability">${projectObj.available}</p>
+                <footer class="projectLinks">
+                    <a href=${projectObj.url}>URL</a>
+                    <a href=${projectObj.githubUrl}>GitHub Repo</a>
+                </footer>
+            </div>
+        `
+
+    }
+    
+    // if () {
+
+    // }
+    printToDom('projectsBody', cardsToPrint);
+};
+
+createProjectCards(projects);

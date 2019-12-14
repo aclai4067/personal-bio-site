6b40337d5d5570a074efd7982f26588e62db4c23
Ashley Claiborne <languageisaliquid@gmail.com>
2019-12-14 01:34:09 -0600

    installed devicon

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,29 +2,13 @@
 import 'bootstrap';
 import '@fortawesome/fontawesome-free/js/all';
 import '../styles/main.scss';
-// import $ from 'jquery';
 import firebase from 'firebase';
 import apiKeys from './helpers/apiKeys.json';
 import projects from './components/projects/projects';
 
-// const bodyDivs = document.getElementsByClassName('fullPage');
-
-// const toggle = (e) => {
-//   const selectedLink = $(e.target).attr('id');
-//   for (let i = 0; i < bodyDivs.length; i += 1) {
-//     if (selectedLink === bodyDivs[i].classList[1]) {
-//       bodyDivs[i].style.display = 'block';
-//     }
-//     if (selectedLink !== bodyDivs[i].classList[1]) {
-//       bodyDivs[i].style.display = 'none';
-//     }
-//   }
-// };
-
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseConfig);
   projects.createProjectCards();
-//   $('body').on('click', '.navLinks', toggle);
 };
 
 init();
